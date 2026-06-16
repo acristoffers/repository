e3d81721b0a8ff7ef6fdb777a24f61fb115d9de0
Álan Crístoffer <acristoffers@startmail.com>
2026-06-16 14:22:03 +0200

    ter. 16 jun 2026 14:22:03 CEST

--- repo-mgmt/gen-repo-pkgs.fish
+++ repo-mgmt/gen-repo-pkgs.fish
@@ -5,8 +5,9 @@ mkdir -p fedora ubuntu
 DIST_TAG=43 envsubst < repo.rpm.yml | nfpm pkg -f - --packager rpm --target fedora/
 DIST_TAG=44 envsubst < repo.rpm.yml | nfpm pkg -f - --packager rpm --target fedora/
 
+gpg --dearmor <pubkey.asc >pubkey.gpg
 for codename in (grep Codename ../deb/conf/distributions | cut -d' ' -f2)
   CODENAME=$codename envsubst <esousa.sources.t >esousa.sources
   CODENAME=$codename envsubst < repo.deb.yml | nfpm pkg -f - --packager deb --target ubuntu/
 end
-rm esousa.sources
+rm esousa.sources pubkey.gpg

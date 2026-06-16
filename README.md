e7fe4f69c3de0bfd541fbd23e7db0e68007c592a
Álan Crístoffer <acristoffers@startmail.com>
2026-06-16 12:03:06 +0200

    ter. 16 jun 2026 12:03:06 CEST

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fedora
+
+```bash
+sudo dnf install https://acristoffers.github.io/repository/rpm/esousa-repo-1.0.0-1.fc$(rpm -E %fedora).$(rpm -E %_arch).rpm
+```
+
+# Ubuntu
--- update.fish
+++ update.fish
@@ -36,5 +36,6 @@ nix run nixpkgs#createrepo_c -- rpm
 pushd rpm/repodata
 gpg --default-key E18472E83DD7E6B8 --detach-sign --armor repomd.xml
 popd
+nix shell nixpkgs#gnupg nixpkgs#rpm -c bash -c "sudo rpm --import pubkey.asc; rpmsign --macros=rpmmacros --resign rpm/*.rpm"
 
 rm -rf deb-tmp
